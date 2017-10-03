9d5a6a9635b88d3bc58c292585b35d4c0f10e9e0
Alexander Medkov <azeeson@gmail.com>
2017-10-03 21:28:02 +0400

    Added support Tag "Include"

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-
 var loaderUtils = require("loader-utils");
 var assign = require("object-assign");
 var Liquid = require("liquid-node");
-var Engine = new Liquid.Engine;
+var Engine = new Liquid.Engine();
+var Path = require("path");
 
 function getLoaderConfig(context) {
 	var query = loaderUtils.getOptions(context) || {};
-	var configKey = query.config || 'liquid';
-	var config = context.options && context.options.hasOwnProperty(configKey) ? context.options[configKey] : {};
+	var configKey = query.config || "liquid";
+	var config =
+		context.options && context.options.hasOwnProperty(configKey)
+			? context.options[configKey]
+			: {};
 	delete query.config;
 	return assign(query, config);
 }
 
-module.exports = function (content) {
+module.exports = function(content) {
 	if (this.cacheable) this.cacheable();
-	var config = getLoaderConfig(this);
-    var callback = this.async();
-	return Engine.parseAndRender(content, config.data || {}).then(function(result) {
+
+	var root,
+		config = getLoaderConfig(this),
+		callback = this.async();
+
+	if (typeof config.context == undefined) {
+		root = Path.join(this.context || ".");
+	} else {
+		root = Path.join(config.context || ".");
+	}
+
+	Engine.registerFileSystem(new Liquid.LocalFileSystem(root));
+	return Engine.parseAndRender(content, config.data || {}).then(function(
+		result
+	) {
 		return callback(null, result);
 	});
-
-}
+};
 
 module.exports.raw = true;
-  
